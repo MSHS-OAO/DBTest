db7924c616db7ded0c16bc3be76c4eb0fb36a0d3
armando-v <57226186+armando-v@users.noreply.github.com>
2022-07-13 12:44:31 -0400

    Add files via upload

---
+++ connection.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(odbc)
+library(DBI)
+library(dbplyr)
+con <- dbConnect(odbc(),
+                 Driver = "Oracle 21_5 ODBC driver",
+                 Trusted_Connection = "True",
+                 uid = "villea04",
+                 pwd = "qKQvPoSilm21T*qVr")
+table <- tbl(con, "SUMMARY REPO")
