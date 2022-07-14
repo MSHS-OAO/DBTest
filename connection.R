2b4713bbb18166dc2fda92e0450ce6b7311dcb87
armando-v <57226186+armando-v@users.noreply.github.com>
2022-07-14 10:11:12 -0400

    Add files via upload

--- connection.R
+++ connection.R
@@ -2,11 +2,14 @@ library(dplyr)
 library(odbc)
 library(DBI)
 library(dbplyr)
-con <- dbConnect(odbc(),
-                 Driver = "Oracle 21_5 ODBC driver",
-                 Trusted_Connection = "True",
-                 uid = "villea04",
-                 pwd = "qKQvPoSilm21T*qVr")
+# con <- dbConnect(odbc(),
+#                  Driver = "Oracle 21_5 ODBC driver",
+#                  Trusted_Connection = "True",
+#                  uid = "villea04",
+#                  pwd = "qKQvPoSilm21T*qVr")
+
+
+con <- dbConnect(odbc(), "OAO Cloud DB", timeout = 30)
 table <- tbl(con, "SUMMARY REPO")
 
 # print(odbc::odbcListDrivers())
